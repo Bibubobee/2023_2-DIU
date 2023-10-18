1503e78b189095c0cfbc56327040db1633da2575
Bibubobee <gaboenix.2@gmail.com>
2023-10-18 12:52:31 -0300

    Added Home page (very barebones)

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.css"
+
+function Home(){
+    return(
+        <div className="Home">
+            <div className="Banner">
+                <div className="Title">ADMISION</div>
+                <div>Admision</div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;

489759ab105cd9b50629982a595106ed965a7ec3
Bibubobee <gaboenix.2@gmail.com>
2023-10-18 14:10:15 -0300

    added career table to homepage

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+function Footer(){
+    return (
+        <footer className="footer">
+            <div className="footer-body">USM.cl</div>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,11 +2,71 @@ import "./Home.css"
 
 function Home(){
     return(
-        <div className="Home">
-            <div className="Banner">
-                <div className="Title">ADMISION</div>
-                <div>Admision</div>
+        <div className="home">
+            <div className="banner">
+                <div className="title">ADMISION</div>
+                <div className="breadcrumbs">Admision (en vola poner debajo del banner?)</div>
             </div>
+
+            <div className="title-carreras">Carreras (Pregrado?)</div>
+            <table> 
+                <tbody>
+                    <tr>
+                        <th>CARRERA</th>
+                        <th>CAMPUS*</th>
+                        <th>JORNADA</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a href="*">Arquitectura</a>
+                        </td>
+                        <td>San Joaquín y Casa Central</td>
+                        <td>Diurna y Vespertina</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a href="*">Ingeniería Civil Eléctrica</a>
+                        </td>
+                        <td>San Joaquín</td>
+                        <td>Diurna</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a href="*">Ingenieria Civil Informática</a>
+                        </td>
+                        <td>San Joaquín y Casa Central</td>
+                        <td>Diurna y Vespertina</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a href="*">Ingeniería Civil Matemática</a>
+                        </td>
+                        <td>San Joaquín</td>
+                        <td>Diurna</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a href="*">Ingeniería Civil Química</a>
+                        </td>
+                        <td>San Joaquín</td>
+                        <td>Diurna</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a href="*">Ingeniería Civil Telemática</a>
+                        </td>
+                        <td>San Joaquín</td>
+                        <td>Diurna</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a href="*">Ingeniería Civil Comercial</a>
+                        </td>
+                        <td>San Joaquín</td>
+                        <td>Diurna</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
